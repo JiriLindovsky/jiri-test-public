ac4b88f725f66fb8795ae20461502ae6b3697dc4
JiriLindovsky <jiri.lindovsky.jr@seznam.cz>
2022-10-02 17:54:41 +0200

    lecture 11 expressions

---
+++ 11/app.ts
@@ -0,0 +1,29 @@
+declare var angular: any;
+
+(function(){
+    'use strict';
+    
+
+    angular.module('App11',[])
+    .controller('Controller11',Controller11);
+    Controller11.$inject=['$scope', '$filter'];
+    function Controller11($scope,$filter){
+       $scope.name="Yakkov";
+        $scope.stateOfBeing="hungry";
+
+       $scope.displayMessage=function () {
+           return "Yakkow eats!";   
+
+        
+       }
+       $scope.feedYakkov=function(){
+        if ($scope.stateOfBeing=="hungry")
+        $scope.stateOfBeing="fed";
+        else
+        $scope.stateOfBeing="hungry";
+       }
+      
+       
+    };
+       
+})();
